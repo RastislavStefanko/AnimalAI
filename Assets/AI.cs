e15d7fc57cbc7f84bd89e121b7987c2333ad8423
rasto <rastislavstefanko1@gmail.com>
2017-03-04 20:55:40 +0100

    +added giraffe animation

---
+++ Assets/AI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+
+    public GameObject Player;
+    float MoveSpeed = 4;
+    float MaxDist = 10;
+    float MinDist = 5;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //transform.LookAt(Player.transform, Vector3.left);
+        transform.LookAt(Vector3.zero);
+        /*if (Vector3.Distance(transform.position, Player.transform.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.transform.position) <= MaxDist)
+            {
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }*/
+    }
+}
