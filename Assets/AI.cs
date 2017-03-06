33f356d7166add193592a46a20563d6bce495a89
rasto <rastislavstefanko1@gmail.com>
2017-03-06 14:48:52 +0100

    +added drink sequence, drink animation

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AI : MonoBehaviour {
+public class AI : MonoBehaviour
+{
 
     public GameObject Player;
 
@@ -16,10 +17,14 @@ public class AI : MonoBehaviour {
 
     private GameObject currentDestination;
     public float hunger = 100;
+    public float thirst = 100;
 
     private Animator anim;
     public bool eat { get; set; }
     public float eatTime = 4;
+
+    public bool drink { get; set; }
+    public float drinkTime;
 
     public float waitUntilDone = 0;
 
@@ -38,21 +43,33 @@ public class AI : MonoBehaviour {
         if (waitUntilDone <= 0)
         {
             planes = GeometryUtility.CalculateFrustumPlanes(sight);
-            if (hunger < 30)
+            if (hunger < 0)
             {
-                currentDestination = gameObject;
-                if (currentDestination.tag != "leafs")
+                if (!eat)
                 {
                     FindFood();
                 }
-
-                if (currentDestination.tag == "leafs" && eat)
+                else
                 {
                     Eat();
                 }
             }
+            else if (thirst < 0)
+            {
+                currentDestination = gameObject;
+                if (currentDestination.tag != "water")
+                {
+                    FindWater();
+                }
+
+                if (currentDestination.tag == "water" && drink)
+                {
+                    DrinkWater();
+                }
+            }
             else
             {
+                anim.SetBool("drink", false);
                 anim.SetBool("eat", false);
                 currentDestination = waypoints[0];
             }
@@ -62,6 +79,7 @@ public class AI : MonoBehaviour {
         {
             waitUntilDone -= Time.deltaTime;
         }
+        thirst -= Time.deltaTime * 0.1f;
         hunger -= Time.deltaTime;
     }
 
@@ -76,7 +94,10 @@ public class AI : MonoBehaviour {
 
     void FindFood()
     {
-            GameObject[] leafs = GameObject.FindGameObjectsWithTag("leafs");
+        GameObject[] leafs = GameObject.FindGameObjectsWithTag("leafs");
+        if (currentDestination.tag != "leafs")
+        {
+            currentDestination = gameObject;
             foreach (GameObject leaf in leafs)
             {
                 if (GeometryUtility.TestPlanesAABB(planes, leaf.GetComponent<Collider>().bounds))
@@ -85,11 +106,55 @@ public class AI : MonoBehaviour {
                 }
             }
 
-            if(currentDestination.tag != "leafs")
+            if (currentDestination.tag != "leafs")
             {
-                transform.Rotate(0,-1, 0);
+                transform.Rotate(0, -1, 0);
             }
-        
+        }
+        else
+        {
+            
+            foreach (GameObject leaf in leafs)
+            {
+                if (GeometryUtility.TestPlanesAABB(planes, leaf.GetComponent<Collider>().bounds))
+                {
+                    
+                    if (Vector3.Distance(currentDestination.transform.position, transform.position) >= Vector3.Distance(leaf.transform.position, transform.position))
+                    {
+                        currentDestination = leaf;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    void DrinkWater()
+    {
+        currentDestination = gameObject;
+        thirst = 100;
+        anim.SetBool("drink", true);
+        waitUntilDone = drinkTime;
+        drink = false;
+    }
+
+    void FindWater()
+    {
+        GameObject[] waterSources = GameObject.FindGameObjectsWithTag("water");
+        foreach (GameObject water in waterSources)
+        {
+            if (GeometryUtility.TestPlanesAABB(planes, water.GetComponent<Collider>().bounds))
+            {
+                currentDestination = water;
+            }
+        }
+
+        if (currentDestination.tag != "water")
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -103,10 +168,14 @@ public class AI : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("hit");
         if (collision.transform.tag == "leafs")
         {
             eat = true;
+        }
+
+        if (collision.transform.tag == "water")
+        {
+            drink = true;
         }
     }
 
@@ -115,6 +184,11 @@ public class AI : MonoBehaviour {
         if (collision.transform.tag == "leafs")
         {
             eat = false;
+        }
+
+        if (collision.transform.tag == "water")
+        {
+            drink = false;
         }
     }
 }
